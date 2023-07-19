5124f1b2a871960f1033f91acec6c398103fe709
tic-fhce <tic.fhce@gmail.com>
2023-07-18 20:03:24 -0400

    Segundo Commit Mejoramiento de modulo unidad

--- src/main/java/com/fhce/unidad/UnidadApplication.java
+++ src/main/java/com/fhce/unidad/UnidadApplication.java
@@ -4,7 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UnidadApplication {
+public class UnidadApplication /*extends SpringBootServletInitializer*/{
+
+	/*
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(UnidadApplication.class);
+	} para produccion*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(UnidadApplication.class, args);
